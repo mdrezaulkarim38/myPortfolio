2375e233e32e6d0acd8e79a8043692c51f24059c
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-26 13:30:13 +0600

    update code

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="m-0 p-0">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
