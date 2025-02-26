c2efa9874447b112ca20c20d4d6ad6d41fae91d8
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-26 20:46:16 +0600

    change

--- portfolio/src/pages/Contact.tsx
+++ portfolio/src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
   const form = useRef<HTMLFormElement | null>(null);
