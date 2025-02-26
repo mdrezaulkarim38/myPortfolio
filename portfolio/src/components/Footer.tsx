74d8ccf8daafa5c35c2ce7930a7661b6d087abe3
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-26 13:12:15 +0600

    new project Testing

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,9 +1,16 @@
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <footer className="bg-white dark:bg-gray-900 m-4">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025 {" "}
+          <a href="" className="hover:underline">
+            SupermanShuvo
+          </a>
+          . All Rights Reserved.
+        </span>
+    </footer>
+  );
+};
 
 export default Footer;
--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -63,7 +63,7 @@ const Header = () => {
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  About
+                  Resume
                 </a>
               </li>
               <li>
@@ -71,15 +71,7 @@ const Header = () => {
                   href="#"
                   className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Pricing
+                  Project
                 </a>
               </li>
               <li>
