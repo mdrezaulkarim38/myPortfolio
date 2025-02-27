2d99124bb8f3d9f7fe06a8519ca812571294d43e
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-27 11:04:40 +0600

    done Home, About, Contact only project and email sending left

--- portfolio/src/pages/Project.tsx
+++ portfolio/src/pages/Project.tsx
@@ -1,6 +1,14 @@
+import BackgroundImage from "./../assets/backgroundImage.jpg";
+
 function Project() {
     return (
-        <section className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-100 text-center pt-25 pb-10">
+        <section className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-100 text-center pt-25 pb-10"
+        style={{
+            backgroundImage: `url(${BackgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}>
             <div className="max-w-3xl mx-auto bg-white rounded-lg shadow-lg p-8 transition-opacity duration-1000">
                 <h2>Projects</h2>
             </div>
